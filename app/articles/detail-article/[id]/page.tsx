a008aea22e12cc3bc1a856a040cc5bfc6454eb58
SRBL <aleeexis.1995@icloud.com>
2024-07-31 17:24:00 +0200

    forget last commit

--- app/articles/detail-article/[id]/page.tsx
+++ app/articles/detail-article/[id]/page.tsx
@@ -4,14 +4,11 @@
 import { Articles } from '@/interfaces/article.interface';
 
 // Libs Next
+import Image from 'next/image';
 import { useParams } from 'next/navigation';
 
 // Libs React
 import { useEffect, useState } from 'react';
-
-// Components
-import ArticleContent from '@/components/ui/article/ArticleContent.component';
-import ArticleHeader from '@/components/ui/article/ArticleHeader.component';
 
 // Helpers
 import Routes from '@/enums/routes.enum';
@@ -49,6 +46,8 @@ const ArticleDetailPage = () => {
     }
   }, [articleId]);
 
+  useEffect(() => {}, [article]);
+
   if (isLoading) {
     return (
       <div className="flex justify-center items-center h-screen">
@@ -69,8 +68,42 @@ const ArticleDetailPage = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <ArticleHeader title={title} img={img} />
-      <ArticleContent sequences={sequence_article} />
+      <h3 className="text-3xl mb-4">{title}</h3>
+      <div className="relative h-48 lg:h-[90%] flex justify-center items-center">
+        <Image
+          src={img ? `/img/${img}` : '/img/logo.jpg'}
+          alt={title}
+          width={1000}
+          height={700}
+          objectFit="cover"
+          className="shadow-xs-light rounded-lg"
+        />
+      </div>
+      <div className="p-4">
+        {sequence_article && sequence_article.length > 0 ? (
+          sequence_article.map((sequence) => (
+            <div key={sequence.id} id={sequence.id} className="mb-8">
+              <h3 className="text-2xl font-bold mb-4">{sequence.title}</h3>
+              <p className="text-lg">{sequence.containt}</p>
+              {sequence.img && (
+                <div className="mt-4 flex justify-center items-center">
+                  <Image
+                    src={
+                      sequence.img ? `/img/${sequence.img}` : '/img/logo.jpg'
+                    }
+                    alt={`Image for sequence: ${sequence.title}`}
+                    width={550}
+                    height={350}
+                    className="rounded-lg"
+                  />
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          <div>Aucune séquence trouvée.</div>
+        )}
+      </div>
     </div>
   );
 };
